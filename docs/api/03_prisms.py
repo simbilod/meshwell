8ddad1df07954a58bbe6f461645458acd086fc99
simbilod <sb30@princeton.edu>
2023-05-06 21:31:00 -0400

    update 03

--- docs/api/03_prisms.py
+++ docs/api/03_prisms.py
@@ -12,15 +12,19 @@
 #     name: python3
 # ---
 
-# # Introduction
+# # Prisms
+#
+# Meshwell can extrude polygonal entities (not already meshed) in the z-direction to define complex 3D shapes.
+#
+# This extrusion is coupled to morphological grow/shrink operations ("buffers") to define complex shapes.
+#
+# THe Prism object takes in a (Multi)Polygon and a dict of z-values: buffer-values. A buffer value of 0 keeps the polygon intact; a negative value shrink the polygon by this amounts; and a positive value grows the polygon similarly.
 
 # + tags=["hide-input"]
 import shapely
 from shapely.plotting import plot_polygon
 import matplotlib.pyplot as plt
 import gmsh
-from meshwell.polysurface import PolySurface
-from skfem.visuals.matplotlib import draw_mesh2d
 from skfem.io.meshio import from_meshio
 import meshio
 from skfem.visuals.matplotlib import draw_mesh3d
@@ -28,83 +32,30 @@ from meshwell.prism import Prism
 
 # -
 
-# Meshwell is a Python wrapper around GMSH that provides:
-#
-# (1) a Prism class that simplifies, to the point of automating, the definition of solids from arbitrary (multi)polygons with "buffered" extrusions
-#
-# For instance, consider some complicated polygon resulting from some upstream calculation:
+# First define some polygon:
 
 # +
-
-polygon_with_holes = shapely.Polygon(
-    [[-2, -2], [3, -2], [3, 2], [-2, 2], [-2, -2]],
-    holes=(
-        [
-            [0.0, 0.0],
-            [1.0, 1.0],
-            [2.0, 0.0],
-            [1.0, -1.0],
-            [0.0, 0.0],
-        ],
-    ),
-)
-polygon_with_holes_boolean = shapely.union(
-    polygon_with_holes - shapely.Point(-2, -2).buffer(2), shapely.Point(0, 2).buffer(1)
-)
+polygon = shapely.Point(0, 0).buffer(2)
 
 fig = plt.figure()
 ax = fig.add_subplot()
-plot_polygon(polygon_with_holes_boolean, ax=ax, add_points=False)
+plot_polygon(polygon, ax=ax, add_points=False)
 plt.show()
-# -
 
-# Meshwell's PolySurface can easily convert this to a 2D GMSH entity:
 
 # +
-
-# Some GMSH boilerplate
-gmsh.clear()
-gmsh.initialize()
-occ = gmsh.model.occ
-
-# This package
-poly2D = PolySurface(polygons=polygon_with_holes_boolean, model=occ)
-
-# More GMSH boilerplate
-occ.synchronize()
-gmsh.option.setNumber("General.Terminal", 0)
-gmsh.model.mesh.generate(2)
-gmsh.write("mesh2D.msh")
-
-# Plotting courtesy of scikit-fem
-mesh = from_meshio(meshio.read("mesh2D.msh"))
-
-draw_mesh2d(mesh)
-# -
-
-# Given a planar polygon, it is also possible to extrude with arbitrary grow and shrink patterns as a function of the z-coordinates, using a dictionary of buffer values:
-
-# +
-
-polygon1 = shapely.Point(0, 0).buffer(2)
-polygon2 = shapely.Point(4, 0).buffer(1)
-polygon = shapely.MultiPolygon(
-    [polygon1, polygon2]
-)  # showing a MultiPolygon input for flexibility
-
 # Combine with "buffers" to richly extrude in 3D as a Prism
 buffers = {
-    -2.0: 1.0,  # z-coordinate: buffer (shrink or grow) to apply to original polygon
-    -1.0: 0.0,
-    0.0: -0.5,
-    0.2: 0.0,
+    -2.0: 1.0,  # at z = -2, grow the polygon by 1 unit
+    -1.0: 0.0,  # at z = -1, use the base polygon
+    0.0: -0.5,  # at z = 0.0, shrink the base polygon by 1/2 a unit
+    0.2: 0.0,  # etc.
     0.5: 1.0,
     0.8: 0.0,
     1.0: -0.5,
 }
 
 # Some GMSH boilerplate
-gmsh.clear()
 gmsh.initialize()
 occ = gmsh.model.occ
 
