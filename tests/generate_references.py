21d356016be30397065f2db18e7dcb7330690d86
Simon Bilodeau <sb30@princeton.edu>
2024-09-12 22:31:58 +0000

    generate references script

---
+++ tests/generate_references.py
@@ -0,0 +1,26 @@
+import gmsh
+from difflib import unified_diff
+from pathlib import Path
+import os
+import shutil
+import pytest
+
+if __name__ == "__main__":
+
+    # Delete existing references
+    exec_dir_name = "./"
+    save_dir_name = "./references/"
+    test = os.listdir(save_dir_name)
+
+    for item in test:
+        if item.endswith(".msh"):
+            os.remove(os.path.join(dir_name, item))
+
+    # Run the tests to generate the files
+    pytest.main()
+
+    # # Rename the references
+    test = os.listdir(exec_dir_name)
+    for item in test:
+        if item.endswith(".msh"):
+            os.rename(item, f"{save_dir_name}/{item[:-4]}.reference.msh")
