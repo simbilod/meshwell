564a6268f724bae6d24f7e7cb7d876a4ed92e2c8
simbilod <sb30@princeton.edu>
2024-11-23 13:50:16 -0800

    fix tests CI

--- tests/generate_references.sh
+++ tests/generate_references.sh
@@ -5,6 +5,7 @@ uv venv --python=3.11
 source .venv/bin/activate
 
 # Install published version of the package in editable mode
+sudo apt-get install libglu1-mesa
 uv pip install meshwell[dev]
 
 # Execute all Python files in the current directory
