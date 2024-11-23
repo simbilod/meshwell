6c7037fd54b6a4a72c5031a3bd53264c2227a879
simbilod <sb30@princeton.edu>
2024-11-23 12:50:59 -0800

    fix reference generation

--- tests/generate_references.py
+++ tests/generate_references.py
@@ -5,13 +5,13 @@ from meshwell.config import PATH
 if __name__ == "__main__":
     # Delete existing references
     exec_dir_name = "./"
+    os.makedirs(PATH.references, exist_ok=True)
     test = os.listdir(PATH.references)
-
     for item in test:
         if item.endswith(".msh"):
             os.remove(os.path.join(PATH.references, item))
 
-    # Run the tests to generate the files
+    # Run the tests to generate the files (tests will fail)
     pytest.main()
 
     # # Rename the references
