1a0a887880b3664cab8a6956ce381e383bc51966
simbilod <sb30@princeton.edu>
2024-11-23 12:48:35 -0800

    add script to generate references from pypi version

---
+++ tests/generate_references.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create and activate virtual environment
+uv venv --python=3.11
+source .venv/bin/activate
+
+# Install published version of the package in editable mode
+uv pip install meshwell[dev]
+
+# Execute all Python files in the current directory
+python generate_references.py
