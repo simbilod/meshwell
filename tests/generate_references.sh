1b9bd7bdf60303f03dccdbcc3230ca305a79f278
simbilod <sb30@princeton.edu>
2024-12-07 23:36:00 -0800

    point to last commit for tets

--- tests/generate_references.sh
+++ tests/generate_references.sh
@@ -6,7 +6,7 @@ source .venv/bin/activate
 
 # Install published version of the package in editable mode
 sudo apt-get install libglu1-mesa
-uv pip install git+https://github.com/simbilod/meshwell.git@4e7c483307d76768de2f45f92698911cb8f8ae54[dev]
+uv pip install git+https://github.com/simbilod/meshwell.git@45571bf13b73a5a11bf7a5e4dbda99cb4a5dc91e[dev]
 
 # Execute all Python files in the current directory
 python generate_references.py --references-path ./references/
