37e5c650bcfa17fedbb8b84fee6880f3b372a460
simbilod <sb30@princeton.edu>
2024-12-30 10:27:59 -0800

    comment

--- tests/generate_references.sh
+++ tests/generate_references.sh
@@ -4,7 +4,7 @@
 uv venv --python=3.11
 source .venv/bin/activate
 
-# Install published version of the package in editable mode
+# Install reference version of the package in editable mode
 sudo apt-get install libglu1-mesa
 uv pip install git+https://github.com/simbilod/meshwell.git@a72f6dfaf6b3b89d1ff7b6dd848524e803fddda2[dev]
 
