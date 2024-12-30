2c198ca76e04ef1bd752828c6fded2c8771d49c2
simbilod <sb30@princeton.edu>
2024-12-30 10:48:18 -0800

    add test fuse case

--- tests/test_fuse.py
+++ tests/test_fuse.py
@@ -7,6 +7,8 @@ from meshwell.model import Model
 import numpy as np
 from meshwell.utils import compare_meshes
 from pathlib import Path
+from shapely.geometry import box
+from meshwell.resolution import ThresholdField
 
 
 def test_fuse():
@@ -61,5 +63,47 @@ def test_fuse():
     compare_meshes(Path("mesh_unfused.msh"))
 
 
+def test_different_resolutions_same_physical():
+    model = Model()
+
+    # Create two different boxes
+    box1 = box(0, 0, 1, 1)
+    box2 = box(2, 0, 3, 1)
+
+    # Create resolution specifications
+    resolution1 = ThresholdField(
+        apply_to="surfaces", sizemin=0.1, sizemax=0.5, distmax=0.2
+    )
+
+    resolution2 = ThresholdField(
+        apply_to="surfaces", sizemin=0.2, sizemax=1.0, distmax=0.4
+    )
+
+    # Create two polysurfaces with same physical name but different resolutions
+    surface1 = PolySurface(
+        polygons=box1,
+        model=model,
+        physical_name="test_surface",
+        resolutions=[resolution1],
+    )
+
+    surface2 = PolySurface(
+        polygons=box2,
+        model=model,
+        physical_name="test_surface",
+        resolutions=[resolution2],
+    )
+
+    # Mesh the model
+    model.mesh(
+        entities_list=[surface1, surface2],
+        default_characteristic_length=0.5,
+        filename="test_different_resolutions_same_physical.msh",
+    )
+
+    compare_meshes(Path("test_different_resolutions_same_physical.msh"))
+
+
 if __name__ == "__main__":
-    test_fuse()
+    # test_fuse()
+    test_different_resolutions_same_physical()
