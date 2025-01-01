87cb7adc4bb546bca22a0d7cf679d2c5a3cd0937
simbilod <sb30@princeton.edu>
2025-01-01 14:45:02 -0800

    update ref commit

--- tests/generate_references.sh
+++ tests/generate_references.sh
@@ -6,7 +6,7 @@ source .venv/bin/activate
 
 # Install reference version of the package in editable mode
 sudo apt-get install libglu1-mesa
-uv pip install git+https://github.com/simbilod/meshwell.git@d8dc6d5f51d1fcb58f4141c7a39344c932deb215[dev]
+uv pip install git+https://github.com/simbilod/meshwell.git@af140b69f2d563beed2c5389d27724d77a3429bb[dev]
 
 # Execute all Python files in the current directory
 python generate_references.py --references-path ./references/
