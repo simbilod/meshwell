1898a8cb20e0ac72adc904b07774248a00734b0e
Simon Bilodeau <bilodeaus@google.com>
2026-03-20 07:08:43 +0000

    repin tests

--- tests/generate_references.sh
+++ tests/generate_references.sh
@@ -7,7 +7,7 @@ source .venv/bin/activate
 # Install reference version of the package
 sudo apt-get install libglu1-mesa
 export PYTHONPATH=""
-uv pip install git+https://github.com/simbilod/meshwell.git@05daeb1daaea416e7b019ee7d93a586ee1eac1cf[dev]
+uv pip install git+https://github.com/simbilod/meshwell.git@26d1c66de14972b17082525743231efcb74da73f[dev]
 
 # Execute all Python files in the current directory
 python generate_references.py --references-path ./references/
