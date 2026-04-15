2f2324dd346e284bd6dbd64b9e73d194e386c157
Simon Bilodeau <bilodeaus@google.com>
2026-04-15 00:22:24 +0000

    updates refs due to new rounding and fusing

--- tests/generate_references.sh
+++ tests/generate_references.sh
@@ -7,7 +7,7 @@ source .venv/bin/activate
 # Install reference version of the package
 sudo apt-get install libglu1-mesa
 export PYTHONPATH=""
-uv pip install git+https://github.com/simbilod/meshwell.git@0a37a9bf03c9bd1fd5a1cce61791b9e945dc68e71[dev]
+uv pip install git+https://github.com/simbilod/meshwell.git@0a4f7b59825a91bdabf35ad6de258dbbbc4ea3d68[dev]
 
 # Execute all Python files in the current directory
 python generate_references.py --references-path ./references/
