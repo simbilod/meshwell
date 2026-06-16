8f0084b9d5739c7e22deaf9c43c692b8179ff35a
simbilod <sb30@princeton.edu>
2026-06-16 00:15:05 -0700

    build: read __version__ from importlib.metadata

--- meshwell/__init__.py
+++ meshwell/__init__.py
@@ -1,6 +1,7 @@
 """meshwell - OpenCASCADE-based CAD and GMSH-based meshing for integrated photonics and beyond."""
 
 import logging
+from importlib.metadata import PackageNotFoundError, version
 
 from meshwell.orchestrator import generate_mesh
 from meshwell.utils import deserialize
@@ -9,7 +10,10 @@ from meshwell.utils import deserialize
 # configuring a handler on the "meshwell" logger.
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = "2.3.6"
+try:
+    __version__ = version("meshwell")
+except PackageNotFoundError:  # running from a source tree without an install
+    __version__ = "0.0.0"
 __author__ = "Simon Bilodeau <sb30@princeton.edu>"
 
 __all__ = ["deserialize", "generate_mesh"]
